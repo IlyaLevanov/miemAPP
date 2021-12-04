23370f27ab7ca46790665cbb33547a9ab6304d89
mokofman <mokofman@miem.hse.ru>
2021-12-05 00:35:22 +0300

    search+moreInfo

--- MIEMApp/MIEMApp/Projects/ProjectsScreenLoad.swift
+++ MIEMApp/MIEMApp/Projects/ProjectsScreenLoad.swift
@@ -9,7 +9,7 @@ import UIKit
 
 private let padding = Brandbook.Paddings.normal
 
-final class ProjectsScreenLoad: UIViewController, ScreenPayload, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+final class ProjectsScreenLoad: UIViewController, ScreenPayload, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate, UISearchResultsUpdating {
   
 
   private unowned let wireframe: Wireframe
@@ -23,6 +23,11 @@ final class ProjectsScreenLoad: UIViewController, ScreenPayload, UICollectionVie
   var types: [String]?
   var status: [String]?
   var vacancies: Bool?
+  
+  var collectionView: UICollectionView?
+
+  var isFiltering = false
+  var filteredProjects = [ProjectsListModel]()
   
   init(wireframe: Wireframe, bottomInset: Variable<CGFloat>, refreshAction: @escaping () -> Void) {
     self.wireframe = wireframe
@@ -42,32 +47,68 @@ final class ProjectsScreenLoad: UIViewController, ScreenPayload, UICollectionVie
   lazy var search: UISearchController = {
       let search = UISearchController()
       search.searchBar.placeholder = "Поиск по проекту"
-//      search.searchResultsUpdater = self
+      search.searchResultsUpdater = self
       
       search.obscuresBackgroundDuringPresentation = false
       search.searchBar.sizeToFit()
       search.searchBar.searchBarStyle = .default
-//      search.searchBar.delegate = self
+      search.searchBar.delegate = self
       return search
   }()
   
-  
-  
-  
-  
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    
+    if isFiltering {
+      return filteredProjects.count
+    }
+    else {
     return model?.count ?? 0
+    }
   }
+  
+  func updateSearchResults(for searchController: UISearchController) {
+    
+    let searchText = searchController.searchBar.text!
+    if !searchText.isEmpty {
+      isFiltering = true
+      filteredProjects.removeAll()
+      for project in model! {
+        
+        let idToString = String(project.id)
+        
+        if project.head.lowercased().contains(searchText.lowercased()) || project.typeDesc.lowercased().contains(searchText.lowercased()) || project.nameRus.lowercased().contains(searchText.lowercased()) || idToString.contains(searchText) {
+          filteredProjects.append(project)
+        }
+      }
+    } else {
+      isFiltering = false
+      filteredProjects.removeAll()
+      filteredProjects = model!
+    }
+    collectionView?.reloadData()
+  }
+  
+  func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+    isFiltering = false
+    filteredProjects.removeAll()
+    collectionView?.reloadData()
+  }
+  
   
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
     return CGSize(width: UIScreen.main.bounds.width - padding, height: 350)
   }
   
   func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    print("select")
     print(indexPath)
     print(model![indexPath.row])
-    let moreVC = MoreProjectScreenLoad(id: model![indexPath.row].id)
+    let id: Int
+    if isFiltering {
+      id = filteredProjects[indexPath.row].id
+    } else {
+      id = model![indexPath.row].id
+    }
+    let moreVC = MoreProjectScreenLoad(id: id)
     self.modalPresentationStyle = .popover
     self.present(moreVC, animated: true, completion: nil)
     
@@ -75,24 +116,35 @@ final class ProjectsScreenLoad: UIViewController, ScreenPayload, UICollectionVie
   
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ListProjectsGalleryCell.reusedId, for: indexPath) as! ListProjectsGalleryCell
-    let currentProject: ProjectsListModel?
-    currentProject = model?[indexPath.row]
-    
-    cell.idLabel.text = "\(currentProject!.id)"
-    cell.nameLabel.text = currentProject?.nameRus
-    cell.headLabel.text = currentProject?.head
-    cell.typeLabel.text = currentProject?.typeDesc
-    cell.statusLabel.text = currentProject!.statusDesc
-    cell.vacanciesLabel.text = ("\(currentProject!.vacancies) вакансий(я)")
-    cell.managerTxt.text = "Руководитель: "
-    cell.typeTxt.text = "Тип: "
-//    cell.backgroundColor = Brandbook.Colors.greenBackround
-    cell.backgroundColor = .white
-    
-    
-    cell.nameLabel.heightAnchor.constraint(equalToConstant: measureFrameForText(currentProject?.nameRus ?? "").height + 3*padding).isActive = true
-    
-    return cell
+    var currentProject: ProjectsListModel?
+
+        
+        if isFiltering {
+          
+          currentProject = filteredProjects[indexPath.row]
+          
+        } else {
+          
+          currentProject = model?[indexPath.row]
+          
+        }
+        
+        
+        cell.idLabel.text = "\(currentProject!.id)"
+        cell.nameLabel.text = currentProject?.nameRus
+        cell.headLabel.text = currentProject?.head
+        cell.typeLabel.text = currentProject?.typeDesc
+        cell.statusLabel.text = currentProject!.statusDesc
+        cell.vacanciesLabel.text = ("\(currentProject!.vacancies) вакансий(я)")
+        cell.managerTxt.text = "Руководитель: "
+        cell.typeTxt.text = "Тип: "
+    //    cell.backgroundColor = Brandbook.Colors.greenBackround
+        cell.backgroundColor = .white
+        
+        
+        cell.nameLabel.heightAnchor.constraint(equalToConstant: measureFrameForText(currentProject?.nameRus ?? "").height + 3*padding).isActive = true
+        
+        return cell
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -127,22 +179,20 @@ final class ProjectsScreenLoad: UIViewController, ScreenPayload, UICollectionVie
     
     let layout = UICollectionViewFlowLayout()
     layout.scrollDirection = .vertical
-    
     self.view.backgroundColor = Brandbook.Colors.grey
+    collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    collectionView?.delegate = self
+    collectionView?.dataSource = self
+    collectionView?.register(ListProjectsGalleryCell.self, forCellWithReuseIdentifier: ListProjectsGalleryCell.reusedId)
+    collectionView?.translatesAutoresizingMaskIntoConstraints = false
     
-    var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-    collectionView.delegate = self
-    collectionView.dataSource = self
-    collectionView.register(ListProjectsGalleryCell.self, forCellWithReuseIdentifier: ListProjectsGalleryCell.reusedId)
-    collectionView.translatesAutoresizingMaskIntoConstraints = false
-    
-    self.view.addSubview(collectionView)
-    collectionView.backgroundColor = .white
-    collectionView.topAnchor.constraint(equalTo: self.navigationController?.navigationBar.bottomAnchor ?? self.view.topAnchor).isActive = true
-    collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
-    collectionView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-    collectionView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
-    collectionView.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+    self.view.addSubview(collectionView!)
+    collectionView?.backgroundColor = .white
+    collectionView?.topAnchor.constraint(equalTo: self.navigationController?.navigationBar.bottomAnchor ?? self.view.topAnchor).isActive = true
+    collectionView?.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    collectionView?.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+    collectionView?.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+    collectionView?.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
 
 
     
