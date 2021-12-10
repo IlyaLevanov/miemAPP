cfa43c8c778826ef50f1c4c5e23748ae397b0fbd
Nikita Rybakovskiy <nikitarybakovskiy@MacBook-Pro-Nikita.local>
2021-12-10 18:57:06 +0300

    DynamicTextHeight

---
+++ DynamicTextHeight/1/1/CustomViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  1
+//
+//  Created by Melanie Kofman on 07.11.2021.
+//  Copyright © 2021 Melanie Kofman. All rights reserved.
+//
+
+import UIKit
+
+final class CustomViewController: UIViewController {
+    init() {
+      super.init(nibName: nil, bundle: nil)
+    }
+}
+
+
+
